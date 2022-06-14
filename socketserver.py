5487c5840dbde68ef00d90ca58dfa9db37a95a11
judy <judy8889107@gmail.com>
2022-06-14 18:57:38 +0800

    新增argument參數輸入

--- socketserver.py
+++ socketserver.py
@@ -2,13 +2,19 @@
 import _thread
 import socket
 import threading
+import sys
 """AF_INET is the address domain of the 
 socket. This is used when we have an Internet Domain with 
 any two hosts The 2nd context of the code is the type of socket. """
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)  
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 # piece of code to allow IP address & Port
-host="127.0.0.1"
+
+# 預設host為127.0.0.1(方便測試)
+host = "127.0.0.1"
+# 若有輸入參數則為預設參數
+if len(sys.argv)==2:
+    host = sys.argv[1]
 port=5000
 s.bind((host,port))
 s.listen(5)
@@ -23,11 +29,13 @@ def connectNewClient(c):
 def sendToAll(msg,con):
     for client in clients:
         client.send(msg.encode('ascii')) 
-        
+
+    
 while True:
     c,ad=s.accept()
     # Display message when user connects
     print('*Server Connected ')
+    print (host)
     clients.append(c)
     c.send(('Online ('+str(clients.index(c)+1)+')').encode('ascii'))
     _thread.start_new_thread(connectNewClient,(c,))
